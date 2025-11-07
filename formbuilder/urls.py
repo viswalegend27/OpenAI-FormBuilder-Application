b218c5199e1116050a5c076bd2c072e6b13c6933
Viswa v <viswamalar2004@gmail.com>
2025-11-07 18:34:44 +0530

    Added favicon.ico ✅ + Added comments in my voice.js ✅

--- formbuilder/urls.py
+++ formbuilder/urls.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from django.urls import include,path
+from django.shortcuts import redirect
+from django.templatetags.static import static
+
+def favicon_redirect(request):
+    return redirect(static("form_ai/favicon.ico"), permanent=True)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("favicon.ico", favicon_redirect),
     path("", include("form_ai.urls")),
 ]
